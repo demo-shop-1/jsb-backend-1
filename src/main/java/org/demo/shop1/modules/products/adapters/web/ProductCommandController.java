c275f74e2191a828d489ecc989603ca78884847c
Willy Arrojas <willyarrojas@gmail.com>
2025-08-18 16:18:00 +0200

    add endpoint for saving serveral products

--- src/main/java/org/demo/shop1/modules/products/adapters/web/ProductCommandController.java
+++ src/main/java/org/demo/shop1/modules/products/adapters/web/ProductCommandController.java
@@ -1,16 +1,19 @@
 package org.demo.shop1.modules.products.adapters.web;
 
 import org.demo.shop1.modules.products.adapters.dto.ProductSaveRequestDTO;
+import org.demo.shop1.modules.products.adapters.dto.ProductSaveResponseDTO;
 import org.demo.shop1.modules.products.adapters.mappers.ProductMapper;
 import org.demo.shop1.modules.products.domain.services.ProductCommandService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
 
 @Configuration
 @RequiredArgsConstructor
@@ -27,13 +30,27 @@ public class ProductCommandController extends ProductController {
     public RouterFunction<ServerResponse> productRoute() {
         return RouterFunctions.route()
                 .POST("/product/save",
-                        req -> req.bodyToMono(ProductSaveRequestDTO.class)
+                        request -> request.bodyToMono(ProductSaveRequestDTO.class)
                                 .doFirst(() -> startMethod("/product/save"))
                                 .flatMap(ProductMapper::toProduct)
                                 .flatMap(productCommandApplication::createProduct)
                                 .flatMap(ProductMapper::toProductSaveResponse)
                                 .flatMap(productResponseDTO -> ServerResponse.ok().bodyValue(productResponseDTO))
                                 .doOnSuccess(serverResponse -> endMethod("/product/save")))
+                .POST("/product/save/all",
+                        request -> {
+                            Flux<ProductSaveResponseDTO> responses = request.bodyToFlux(ProductSaveRequestDTO.class)
+                                    .doFirst(() -> startMethod("/product/save/all"))
+                                    .flatMap(ProductMapper::toProduct)
+                                    .flatMap(productCommandApplication::createProduct)
+                                    .flatMap(ProductMapper::toProductSaveResponse);
+
+                            return ServerResponse.ok()
+                                    .contentType(MediaType.APPLICATION_JSON) // streaming JSON
+                                    .body(responses, ProductSaveResponseDTO.class)
+                                    .doOnSuccess(serverResponse -> endMethod("/product/save/all"));
+                        })
+
                 .build();
     }
 }
